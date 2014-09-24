df38e8db55b4227fda254e2c9685d275203d7f10
Cole R Lawrence <cole@zombiehippie.com>
2014-09-24 13:11:27 -0500

    Added displays and keyboard bindings to wasd

--- twentyfortyeight.py
+++ twentyfortyeight.py
@@ -6,8 +6,37 @@ Version: 0.0.1
 
 import tkinter as tk
 
-def key(event):
-    print("pressed", repr(event.char))
+
+class Grid():
+    def __init__(self, update, end, rows, columns):
+        self._update = update
+        self.end = end
+        self.rows = rows
+        self.columns = columns
+        self.cells = []
+        for i in range(rows):
+            self.cells.append([0] * columns)
+
+        self.update()
+
+    def update(self):
+        res = []
+        for arr in self.cells:
+            res += arr
+        self._update(res)
+
+    def shiftUp(self):
+        self.update()
+
+    def shiftDown(self):
+        self.update()
+
+    def shiftLeft(self):
+        self.update()
+
+    def shiftRight(self):
+        self.update()
+
 
 class App(tk.Frame):
     """Primary window for 2048 game display"""
@@ -22,30 +51,77 @@ class App(tk.Frame):
         self.grid()
         self.master.title("2048py")
 
+        self._t_gameStartBtnText = tk.StringVar()
+        self._t_gameStartBtn = tk.Button(self,
+                                         textvariable=self._t_gameStartBtnText,
+                                         command=self.startGame)
+        self._t_gameStartBtnText.set("Start Game")
+        self._t_gameStartBtn.grid()
+
         self._t_scoreLabel = tk.Label(self, text="Start Game")
         self._t_scoreLabel.grid()
         self._t_score = 0
 
         cellGrid = tk.Frame(self, width=480, height=480, padx=20, pady=20)
         cellGrid.grid()
-        cells = []
+        cellVars = []
 
         cellWidth = int(480/fontSize/columns)
         cellHeight = int(480/fontSize/rows*.56)
         print(cellWidth, cellHeight)
         for i in range(rows):
             for j in range(columns):
-                newCell = tk.Label(cellGrid, text="a", width=cellWidth, height=cellHeight, font=self.customFont)
+                newCellVar = tk.StringVar()
+                newCell = tk.Label(cellGrid, textvariable=newCellVar,
+                                   width=cellWidth, height=cellHeight,
+                                   font=self.customFont)
+                newCellVar.set("a")
                 newCell.grid(row=i, column=j)
-                cells.append(newCell)
+                cellVars.append(newCellVar)
 
-        self._t_cells = cells
-        self._t_grid = cellGrid
+        self._t_cells = cellVars
+        self._t_grid = Grid(update=self.updateGridDisplay, end=self.endGame,
+                            rows=rows, columns=columns)
 
-        cellGrid.bind("<Key>", key)
+        self.keyHandlers = {
+            "w": self._t_grid.shiftUp,
+            "a": self._t_grid.shiftLeft,
+            "d": self._t_grid.shiftRight,
+            "s": self._t_grid.shiftDown
+        }
+
+        cellGrid.bind("<Key>", self.keyHandler)
 
         self.winfo_toplevel().bind("<Button-1>", lambda e: cellGrid.focus_set())
         #cellGrid.pack()
+
+        self.gameStarted = False
+        self.gameOver = False
+
+        self.startGame()
+        cellGrid.focus_set()
+
+    def updateGridDisplay(self, numbersArray):
+        print(numbersArray)
+        for n in range(len(numbersArray)):
+            value = numbersArray[n]
+            value = "" if value is 0 else str(value)
+            self._t_cells[n].set(value)
+
+    def startGame(self):
+        if not self.gameStarted and not self.gameOver:
+            self.gameStarted = True
+            self._t_gameStartBtnText.set("Game in progress")
+
+    def endGame(self):
+        if self.gameStarted and not self.gameOver:
+            self.gameOver = True
+            self._t_gameStartBtn["text"] = "Game in progress"
+
+    def keyHandler(self, event):
+        letter = str(event.char)
+        if self.gameStarted and letter in self.keyHandlers:
+            self.keyHandlers[letter]()
 
     def updateScore(self):
         self._scoreLabel["text"] = "Score: " + str(self._score)
